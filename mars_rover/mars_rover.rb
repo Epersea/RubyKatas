8150eb04cd4e580cb8ba69a15819a2df6293d2dd
Elisa <elisa@flexmyroom.com>
2023-07-25 19:25:18 +0200

    Mars Rover: refactor to avoid altering @landing_zone

--- mars_rover/mars_rover.rb
+++ mars_rover/mars_rover.rb
@@ -4,14 +4,14 @@ class MarsRover
 
     def initialize(landing_zone)
         @landing_zone = landing_zone
-        @position = operable_landing_zone
+        @position = operable_landing_zone(@landing_zone)
     end
 
-    def operable_landing_zone
-        @landing_zone = @landing_zone.split(", ")
-        @landing_zone[0] = @landing_zone[0].to_i
-        @landing_zone[1] = @landing_zone[1].to_i
-        @landing_zone
+    def operable_landing_zone(landing_zone)
+        landing_zone = landing_zone.split(", ")
+        landing_zone[0] = landing_zone[0].to_i
+        landing_zone[1] = landing_zone[1].to_i
+        landing_zone
     end
 
     def position
