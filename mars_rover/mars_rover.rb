267fc7f9a32d16b3463dbadad896f7b84123d70e
Elisa <elisa@flexmyroom.com>
2023-07-25 19:03:25 +0200

    Mars Rover: knows how to move forward refactor

--- mars_rover/mars_rover.rb
+++ mars_rover/mars_rover.rb
@@ -2,19 +2,27 @@ class MarsRover
 
     def initialize(landing_zone)
         @landing_zone = landing_zone
-        @position = landing_zone
+        @position = operable_landing_zone
     end
 
-    def landing_zone
+    def operable_landing_zone
+        @landing_zone = @landing_zone.split(", ")
+        @landing_zone[0] = @landing_zone[0].to_i
+        @landing_zone[1] = @landing_zone[1].to_i
         @landing_zone
     end
 
     def position
-        @position
+        @position.join(", ")
     end
 
     def move(command)
-        @position = "1, 5, N"
+        orders = command.split("")
+        orders.each do |order|
+            if order == "M"
+                @position[1] += 1
+            end
+        end
     end
 
 end
