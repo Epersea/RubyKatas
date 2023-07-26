5cbb86d35e1753d9f17e98e6d79f18d06bd19db9
Elisa <elisa@flexmyroom.com>
2023-07-26 17:16:32 +0200

    Mars Rover: refactor (axis names, simplifying of rotation

--- mars_rover/mars_rover.rb
+++ mars_rover/mars_rover.rb
@@ -10,8 +10,8 @@ class MarsRover
     def to_dictionary(zone)
         zone = zone.split(", ")
         zone_to_dictionary = {
-            east_west: zone[0].to_i,
-            north_south: zone[1].to_i,
+            west_east: zone[0].to_i,
+            south_north: zone[1].to_i,
             orientation: zone[2]
         }
 
@@ -19,7 +19,7 @@ class MarsRover
     end
 
     def position
-        "#{@position[:east_west]}, #{@position[:north_south]}, #{@position[:orientation]}"
+        "#{@position[:west_east]}, #{@position[:south_north]}, #{@position[:orientation]}"
     end
 
     def execute(command)
@@ -30,39 +30,36 @@ class MarsRover
             end
 
             if order == "R"
-                rotate_right
+                rotate("right")
             end
 
             if order == "L"
-                rotate_left
+                rotate("left")
             end
         end
     end
 
     def move_forward
         if @position[:orientation] == "N"
-            @position[:north_south] += 1
+            @position[:south_north] += 1
         elsif @position[:orientation] == "S"
-            @position[:north_south] -= 1
+            @position[:south_north] -= 1
         elsif @position[:orientation] == "E"
-            @position[:east_west] += 1
+            @position[:west_east] += 1
         elsif @position[:orientation] == "W"
-            @position[:east_west] -= 1
+            @position[:west_east] -= 1
         end
     end
 
-    def rotate_right
+    def rotate(direction)
         cardinal_index = CARDINAL_POINTS.index(@position[:orientation])
-        cardinal_index = cardinal_index += 1
-        if cardinal_index > 3
-            cardinal_index = 0
-        end
-        @position[:orientation] = CARDINAL_POINTS[cardinal_index]
-    end
 
-    def rotate_left
-        cardinal_index = CARDINAL_POINTS.index(@position[:orientation])
-        cardinal_index = cardinal_index -= 1
+        if direction == "right"
+            cardinal_index = cardinal_index - 3
+        elsif direction == "left"
+            cardinal_index = cardinal_index - 1
+        end
+
         @position[:orientation] = CARDINAL_POINTS[cardinal_index]
     end
 
